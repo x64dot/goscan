daff2fe1f9d63ba53ee76176ac5abcdc9933febc
x64dot <142705403+x64dot@users.noreply.github.com>
2023-08-27 16:05:29 -0400

    Update goscan.go
    This update fixed a runtime panic bug that occurred when the port was reported closed, improves error handling, and enhances user input handling.

--- goscan.go
+++ goscan.go
@@ -10,32 +10,33 @@ import (
 
 func main() {
 	var (
-		ip_target          string
-		port_to_scan       string
-		dummy_port_to_scan int
+		ip_target    string
+		port_to_scan string
 	)
 	fmt.Println("Enter the ip address of the target you want to scan: ")
 	fmt.Scanln(&ip_target)
 	fmt.Println("Enter the port of the ip address you want to scan: ")
-	fmt.Scanln(&dummy_port_to_scan)
+	fmt.Scanln(&port_to_scan)
 
-	if dummy_port_to_scan > 65535 {
-		fmt.Println("[!] Invalid port. You cannot specify a port higher than 65535!")
+	dummy_port_to_scan, err := strconv.Atoi(port_to_scan)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
 		os.Exit(0)
 	}
 
-	port_to_scan = strconv.Itoa(dummy_port_to_scan)
+	if dummy_port_to_scan < 1 || dummy_port_to_scan > 65535 {
+		fmt.Println("[!] Invalid port. You cannot specify a port higher than 65535 or port 0!")
+		os.Exit(0)
+	}
+
 	time_out := time.Second * 3
 	conn, err := net.DialTimeout("tcp", ip_target+":"+port_to_scan, time_out)
 
 	if err != nil {
 		fmt.Printf("[!] Port %v is not open!\n", port_to_scan)
-		fmt.Println("\n\n ", err)
 	} else {
 		fmt.Printf("[+] Port %v is open!\n", port_to_scan)
-
+		defer conn.Close()
 	}
-
-	defer conn.Close()
-
 }
